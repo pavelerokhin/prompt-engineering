48a43f0814918ddff87e71c2c596661ad6dfdc86
Pavel <44894330+pavelerokhin@users.noreply.github.com>
2023-08-20 18:53:40 +0200

    Update thinking_prompt.py

--- text_generation/thinking_prompt.py
+++ text_generation/thinking_prompt.py
@@ -13,6 +13,6 @@ pt_verbose = PromptTemplate(
 When you reply, first find exact quotes in the FAQ relevant to the user's question and write them down word for word inside <thinking></thinking> XML tags.  This is a space for you to write down relevant content explicitly.  Once you are done extracting relevant quotes, answer the question.  Put your answer to the user inside <answer></answer> XML tags."""
 )
 
-print(openai(pt.format(query="I have 4 apples and i give you two of my apples. How many apples do i have left?")))
+print(openai(pt.format(query="I have 4 apples and I give you two of my apples. How many apples do I have left?")))
 print("*"*80)
-print(openai(pt_verbose.format(query="I have 4 apples and i give you two of my apples. How many apples do i have left?")))
+print(openai(pt_verbose.format(query="I have 4 apples and I give you two of my apples. How many apples do I have left?")))
